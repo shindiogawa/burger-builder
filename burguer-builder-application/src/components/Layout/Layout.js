1b8be2b40c09acd26c27aaff68aa45c9104548b6
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-04-14 16:48:38 -0300

    2- Implementation of the burger builder container

--- burguer-builder-application/src/components/Layout/Layout.js
+++ burguer-builder-application/src/components/Layout/Layout.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
-        <footer >
+
+        {/* We can add a footer right here */}
+        {/* <footer >
             <div >© 2020 Copyright:
                 <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
             </div>
-        </footer>
+        </footer> */}
 
     </Aux>
 );

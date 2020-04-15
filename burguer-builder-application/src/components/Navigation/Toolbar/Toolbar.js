931a7ca0e7d597955720e06675fef64629b9a53d
Rodrigo Ogawa <rodrigoshindi@gmail.com>
2020-04-14 21:35:13 -0300

    21- Adding Reusable Navigation Items

--- burguer-builder-application/src/components/Navigation/Toolbar/Toolbar.js
+++ burguer-builder-application/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import classes from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
-
+import NavigationItens from '../NavitagionItems/NavigationItems'
 const toolbar = (props) => (
     <header className={classes.Toolbar}> 
         <div>MENU</div>
         <Logo/>
         <nav>
-            ...
+            <NavigationItens/>
         </nav>
     </header>
 );
